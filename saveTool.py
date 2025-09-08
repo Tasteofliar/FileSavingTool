8008827e448dc92c1bf03e86b7c83a2df7b6fa7c
Tasteofliar <Nightza00777@gmail.com>
2025-09-08 13:02:14 +0700

    Add all function, Tool complete

--- saveTool.py
+++ saveTool.py
@@ -7,12 +7,11 @@ from PySide2.QtWidgets import *
 from shiboken2 import wrapInstance
 import maya.OpenMayaUI as omui
 from . import saveToolfn
+import maya.cmds as cmds
 
 class toolWindow(QDialog):
     def __init__(self, *args, **kwargs):
         super(toolWindow, self).__init__(*args, **kwargs)
-        self.s_window = None
-        self.o_window = None
         self.setWindowTitle('Save Tool')
         self.resize(300,150)
 
@@ -51,7 +50,7 @@ class toolWindow(QDialog):
         self.tool_layout.addWidget(self.shot_label,0,1)
         self.tool_layout.addWidget(self.shot_listWidget,1,1)
         self.tool_layout.addWidget(self.department_label,0,2)       
-        self.tool_layout.addWidget(self.department_combobox,1,2, alignment=Qt.AlignTop)
+        self.tool_layout.addWidget(self.department_combobox,1,2, alignment= Qt.AlignTop)
         self.tool_layout.addWidget(self.version_label,3,0)       
         self.tool_layout.addWidget(self.version_listWidget, 4, 0, 1, 3)
 
@@ -59,6 +58,7 @@ class toolWindow(QDialog):
         self.loadSequences()
         self.seq_listWidget.currentItemChanged.connect(self.loadShots)
         self.shot_listWidget.currentItemChanged.connect(self.loadDepartment)
+        self.department_combobox.currentTextChanged.connect(self.load_versions)
 
     def loadSequences(self):
         if os.path.exists(self.path):
@@ -90,6 +90,26 @@ class toolWindow(QDialog):
                     full_path = os.path.join(shot_path, dmp)
                     if os.path.isdir(full_path):
                         self.department_combobox.addItem(dmp)
+
+    def load_versions(self, department):
+        self.version_listWidget.clear()
+        cur_seq_name = self.seq_listWidget.currentItem()
+        cur_shot_name = self.shot_listWidget.currentItem()
+
+        if cur_seq_name and cur_shot_name == None:
+            return
+
+        seq_name = cur_seq_name.text()
+        shot_name = cur_shot_name.text()
+        dept_path = os.path.join(self.path, seq_name, shot_name, department, "maya", "scenes")
+
+        if os.path.exists(dept_path):
+            for file in sorted(os.listdir(dept_path)):
+                full_path = os.path.join(dept_path, file)
+                if os.path.isfile(full_path) and file.lower().endswith(".ma"):
+                    ver_number = re.findall(r"(v\d{3})", file)
+                    if ver_number:
+                        self.version_listWidget.addItem(ver_number[0])
                   
     def initButtonWidgets(self):
         self.opt_widget = QWidget()
@@ -102,21 +122,23 @@ class toolWindow(QDialog):
         self.open_button = QPushButton('Open')
         self.open_button.clicked.connect(self.openWindowPopup)
         self.cancel_button = QPushButton('Cancel')
-        self.cancel_button.clicked.connect(self.close)
+        self.cancel_button.clicked.connect(close)
         
         self.opt_layout.addWidget(self.save_button)
         self.opt_layout.addWidget(self.open_button)
         self.opt_layout.addWidget(self.cancel_button)
 
     def saveWindowPopup(self):
-        if self.s_window is None:
-            self.s_window = saveWindow(parent = self)
-        self.s_window.show()
+        global s_ui
+        if s_ui is None:
+            s_ui = saveWindow(parent = self)
+        s_ui.show()
 
     def openWindowPopup(self):
-        if self.o_window is None:
-            self.o_window = openWindow(parent = self)
-        self.o_window.show()
+        global o_ui
+        if o_ui is None:
+            o_ui = openWindow(parent = self)
+        o_ui.show()
  
 class saveWindow(QDialog):
     def __init__(self, *args, **kwargs):
@@ -143,6 +165,8 @@ class saveWindow(QDialog):
         base_path = self.parent().path
 
         save_path = saveToolfn.saveFile(seq, shot, department,base_path)
+        self.parent().load_versions(department)
+        self.close()
 
 class openWindow(QDialog):
     def __init__(self, *args, **kwargs):
@@ -156,23 +180,74 @@ class openWindow(QDialog):
         self.open_layout.addWidget(self.set_open_label)
 
         self.confirm_button = QPushButton('Confirm')
+        self.confirm_button.clicked.connect(self.open)
         self.popup_close_button = QPushButton('Cancel')
+        self.popup_close_button.clicked.connect(self.close)
         self.open_layout.addWidget(self.confirm_button)
         self.open_layout.addWidget(self.popup_close_button)
         self.popup_close_button.clicked.connect(self.close)
 
+    def open(self):
+        seq = self.parent().seq_listWidget.currentItem().text()
+        shot = self.parent().shot_listWidget.currentItem().text()
+        department = self.parent().department_combobox.currentText()
+        base_path = self.parent().path
+        version_item = self.parent().version_listWidget.currentItem()
+        if not version_item:
+            QMessageBox.warning(self, "Warning", "Please select a version to open.")
+            return
+
+        version = version_item.text()
+        filename = f"{seq}_{shot}_{department}_{version}.ma"
+
+        if cmds.file(query=True, modified=True):
+            reply = QMessageBox.question(
+                self,
+                "Unsaved Changes",
+                "Current scene has unsaved changes. Do you want to save it before opening a new file?",
+                QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel
+            )
+
+            if reply == QMessageBox.Yes:
+                saveToolfn.saveFile(seq, shot, department, base_path)
+                self.parent().load_versions(department)
+            elif reply == QMessageBox.No:
+                saveToolfn.openFile(seq, shot, department, base_path, filename)
+            elif reply == QMessageBox.Cancel:
+                return  
+
+        saveToolfn.openFile(seq, shot, department, base_path, filename)
+        self.close()
+
+
 def close():
+    global ui, s_ui, o_ui
+    if s_ui:
+        s_ui.close()
+        s_ui = None
+    if o_ui:
+        o_ui.close()
+        o_ui = None
     ui.close()
 
 def run():
-    global ui
+    global ui, s_ui, o_ui
     try:
         ui.close()
+    except:
+        pass
+    try:
+        s_ui.close()
+    except:
+        pass
+    try:
+        o_ui.close()
     except:
         pass
 
     maya_ptr = omui.MQtUtil.mainWindow()
     ptr = wrapInstance(int(maya_ptr), QWidget)
-
     ui = toolWindow(parent = ptr)
+    s_ui = None
+    o_ui = None
     ui.show()
--- saveToolfn.py
+++ saveToolfn.py
@@ -1,14 +1,34 @@
 import os
 import maya.cmds as cmds
+import re
 
 def getSceneFolder(base_path, seq, shot, department):
     return os.path.join(base_path, seq, shot, department, "maya", "scenes")
 
+def getNextVersion(save_dir, seq, shot, department):
+    versions = []
+    if os.path.exists(save_dir):
+        for file in os.listdir(save_dir):
+            match = re.match(rf"{seq}_{shot}_{department}_v(\d+)\.(ma)$", file)
+            if match:
+                versions.append(int(match.group(1)))
+
+    if not versions:
+        return 1 
+    return max(versions) + 1
+
 def saveFile(seq, shot, department, base_path):
     save_dir = getSceneFolder(base_path, seq, shot, department)
-
-    filename = f"{seq}_{shot}_{department}.ma"
+    version_num = getNextVersion(save_dir, seq, shot, department)
+    filename = f"{seq}_{shot}_{department}_v{version_num:03d}.ma"
     save_path = os.path.join(save_dir, filename)
 
     cmds.file(rename=save_path)
-    cmds.file(save=True, type="mayaAscii")  
+    cmds.file(save=True, type="mayaAscii")
+
+def openFile(seq, shot, department, base_path, filename):
+    scene_dir = getSceneFolder(base_path, seq, shot, department)
+    file_path = os.path.join(scene_dir, filename)
+
+    if os.path.exists(file_path):
+        cmds.file(file_path, open=True, force=True)
